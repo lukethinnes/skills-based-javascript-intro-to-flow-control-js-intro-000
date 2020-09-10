89b53a42f39bb2685eca76ade60aaa16b732a0d0
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-10 05:07:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,32 @@
 function basicTeenager(age) {
+  if (age >= '13' && age <= '19' ) {
+    console.log('You are a teenager!')
+  }
 
 }
 
 function teenager(age) {
+  if (age >= '13' && age < '20') {
+    console.log("You are a teenager!")
+  } else {
+    console.log("You are not a teenager")
+  }
 
 }
 
 function ageChecker(age) {
-
+  if (age <= '19' && age >= '13') {
+    console.log("You are a teenager!")
+  }
+  else if (age > '20') {
+    console.log("You are a grownup")
+  }
+  else
+    console.log("You are a kid")
 }
 
 function ternaryTeenager(age) {
-
+  return ("You are " + (age > '12' && age < '20' ? "a teenager" : "not a teenager"))
 }
 
 function switchAge(age) {
